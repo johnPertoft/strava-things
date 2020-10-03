97df5f46dfc5b6733b1b36fca51fdf40f751c918
John Pertoft <john.pertoft@schibsted.com>
2020-10-03 22:42:13 +0200

    Add option to filter out irregularities in activities.

--- combined-routes.py
+++ combined-routes.py
@@ -1,13 +1,18 @@
 import argparse
 import glob
+import math
 import os
 import re
+from typing import List, Tuple
 
 import folium
 
 
-def append_route(m: folium.Map, gpx_file: str) -> None:
-    print(f"Parsing {gpx_file}")
+def _distance(p1: Tuple[float, float], p2: Tuple[float, float]) -> float:
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+def _load_positions(gpx_file: str) -> List[Tuple[float, float]]:
     number_pattern = re.compile("[-]?[0-9]*[.]?[0-9]+")
     positions = []
     with open(gpx_file) as f:
@@ -15,14 +20,42 @@ def append_route(m: folium.Map, gpx_file: str) -> None:
             if line.strip().startswith("<trkpt"):
                 lat, lng = re.findall(number_pattern, line)
                 positions.append((float(lat), float(lng)))
+    return positions
 
-    line = folium.PolyLine(positions, color="orange", tooltip=f"From file {gpx_file}")
-    line.add_to(m)
+
+def append_route(
+    m: folium.Map,
+    gpx_file: str,
+    filter_irregular_paths: bool = True,
+    distance_threshold: float = 0.001,
+    min_positions_per_segment: int = 5
+) -> None:
+    print(f"Parsing {gpx_file}")
+    
+    def add_line(positions):
+        line = folium.PolyLine(positions, color="orange", tooltip=f"From file {gpx_file}")
+        line.add_to(m)
+
+    positions = _load_positions(gpx_file)
+
+    if not filter_irregular_paths:
+        add_line(positions)
+    else:
+        # Find the "more contiguous" segments. I.e. if an activity has two neighboring
+        # positions that are very far apart we split these into two different segments.
+        distances = [_distance(p1, p2) for p1, p2 in zip(positions, positions[1:])]
+        segment_end_indices = [i for i, d in enumerate(distances) if d > distance_threshold]
+        segment_start_indices = [i + 1 for i in segment_end_indices]
+        segment_indices = zip([0] + segment_start_indices, segment_end_indices + [len(positions) - 1])
+        for s, e in segment_indices:
+            segment_positions = positions[s : e + 1]
+            if len(segment_positions) >= min_positions_per_segment:
+                add_line(segment_positions)
 
 
 def main(args):
     m = folium.Map(
-        tiles="CartoDB dark_matter",
+        tiles="CartoDB dark_matter" if not args.light else "Stamen toner",
         prefer_canvas=True,
         max_zoom=20)
 
@@ -51,6 +84,12 @@ if __name__ == "__main__":
         "--output",
         default="combined-routes.html",
         help="The output html file.")
+
+    arg_parser.add_argument(
+        "--light",
+        action="store_true",
+        default=False,
+        help="Whether to output a map with light background.")
 
     main(arg_parser.parse_args())
 
